c01f5181baff8d2c757052c90ad03d6b5cb02105
Avani-xo <130911770+Avani-xo@users.noreply.github.com>
2024-12-01 22:51:11 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+// var generateName = require('sillyname');
+// var sillyName = generateName();
+
+// console.log(`hey silly, I am ${sillyName}`);
+
+import {randomSuperhero} from 'superheroes';
+
+console.log(`I never told you but I am ${randomSuperhero()}`);
